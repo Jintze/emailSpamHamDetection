d20c0566114e2a359bf24e8e50b14a31f4e96a90
Jinzhe Li <32780791+Jintze@users.noreply.github.com>
2020-03-29 17:44:50 -0600

    Update hw2_classify.py

--- hw2_classify.py
+++ hw2_classify.py
@@ -7,7 +7,7 @@ def evaluateEmail():
     files= os.listdir(path)
     # probability_spam_words = {}
     # totalWordsNumSpam = 0
-    n = 0
+    n = 1
     hamEmail = []
     spamEmail = []
     probability_ham_words = {}
@@ -27,13 +27,18 @@ def evaluateEmail():
             # print(type(num))
             probability_spam_words[splitLine[0]] = num
 
+        fileOutput = open("Output" + str(n) + ".txt", "a")
+        fileOutput.writelines("P(Spam, " + words + ')\n')
+
         for x in splitWords:
             # print(probability_spam_words[x])
             if x in probability_spam_words:
                 value = probability_spam_words[x]
                 # probOfSpam += math.log(value)
                 probOfSpam += value
-        
+                fileOutput.writelines("P('" + x + "'|Spam) = " + str(value) + "\n")
+        fileOutput.writelines("log P(Spam, " + words + ") = " + str(math.log(probOfSpam)) + "\n\n")
+
         for line in open("probability_ham_words.txt"): 
             splitLine = line.split(" ")
             splitLine[1] = splitLine[1][:-1]
@@ -41,6 +46,9 @@ def evaluateEmail():
             # print(splitLine)
             # print(type(num))
             probability_ham_words[splitLine[0]] = num
+        
+        
+        fileOutput.writelines("P(Ham, " + words + ')\n')
 
         for x in splitWords:
             # print(probability_spam_words[x])
@@ -48,13 +56,18 @@ def evaluateEmail():
                 value = probability_ham_words[x]
                 # probOfHam += math.log(value)
                 probOfHam += value
+                fileOutput.writelines("P('" + x + "'|Hpam) = " + str(value) + "\n")
+        fileOutput.writelines("log P(Ham, " + words + ") = " + str(math.log(probOfHam)) + "\n\n")
         
         # if probOfHam + math.log(220/259) > probOfSpam + math.log(39/259):
         if probOfHam + 220/259 > probOfSpam + 39/259:
         # if probOfHam > probOfSpam:
             print(file + ' is Ham')
+            fileOutput.writelines("Conclusion: This message is classified as Ham.\n")
         else:
             print(file + ' is Spam')
+            fileOutput.writelines("Conclusion: This message is classified as Spam.\n")
+        n += 1
                    
 
 def main():
